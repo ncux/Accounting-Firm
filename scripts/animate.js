60f3e46d3b4e15e95db8671d836456d37beccaef
Talent Malaba <malabatalent@gmail.com>
2019-03-13 17:17:56 +0800

    Added smooth scrolling animation and transparency to the navbar

---
+++ scripts/animate.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+// Sticky menu background: make it a bit transparent while scrolling
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.7;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Smooth Scrolling
+$('#navbar a, .button').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
